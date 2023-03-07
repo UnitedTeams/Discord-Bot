59a1a4cdd36225fda74a4855f5d3e521bb8861d4
realslimsutton <87341175+realslimsutton@users.noreply.github.com>
2023-03-07 19:57:19 +0000

    Register commands as guild commands

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,4 +1,4 @@
-import { Client, Events } from "discord.js";
+import { Client, Events, REST, Routes } from "discord.js";
 import Commands from "../Commands";
 
 export default (client: Client): void => {
@@ -7,9 +7,36 @@ export default (client: Client): void => {
             return;
         }
 
-        const commands = Commands.map(c => c.data);
-        await client.application.commands.set(commands);
+        await registerCommands();
 
         console.log(`${client.user.tag} is online`);
     });
 }
+
+const registerCommands = async() => {
+    const commands = Commands.map(c => c.data.toJSON());
+
+    const token = process.env.TOKEN;
+    const applicationId = process.env.APPLICATION_ID;
+    const guildId = process.env.GUILD_ID;
+
+    if(!token || !applicationId || !guildId) {
+        console.warn("Secrets are not set in .env. Bot commands will not be registered.");
+        return;
+    }
+
+    const rest = new REST().setToken(token);
+
+    try {
+        console.log(`Registering ${commands.length} bot (/) commands`);
+
+        await rest.put(
+            Routes.applicationGuildCommands(applicationId, guildId),
+            { body: commands }
+        );
+
+        console.log(`Successfully registered bot (/) commands.`);
+    } catch (error) {
+        console.error(error);
+    }
+};
